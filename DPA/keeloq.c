7b1fd365e84cf672d2ec91c4fe388928b969e842
Ben Cherian <cherian.b59@gmail.com>
2024-09-25 23:01:17 +1000

    fix calling to c fnuction ,add ctypes

---
+++ DPA/keeloq.c
@@ -0,0 +1,76 @@
+/*\
+ *  Corrected KeeLoq Encryption and Decryption functions by Ruptor.
+ *  Use at your own risk! This source is not copyrighted.
+ *  Encoder encrypts an all-0 block, decoder decrypts it.
+ *  KeeLoq cipher encrypts 32-bit blocks with 64-bit keys.
+ *  Key is XORed with a 32-bit IV incremented on each use.
+ *  See http://www.keeloq.boom.ru/decryption.pdf for more details.
+ *  KeeLoq algorithm itself is not patented.
+\*/
+#include <stdint.h>
+#include <stdio.h>
+
+#define KeeLoq_NLF		0x3A5C742E
+#define bit(x,n)		(((x)>>(n))&1)
+#define g5(x,a,b,c,d,e)	(bit(x,a)+bit(x,b)*2+bit(x,c)*4+bit(x,d)*8+bit(x,e)*16)
+
+uint32_t	KeeLoq_Encrypt (const uint32_t data, const uint64_t key)
+{
+	uint32_t	x = data, r;
+
+	for (r = 0; r < 528; r++)
+	{
+		x = (x>>1)^((bit(x,0)^bit(x,16)^(uint32_t)bit(key,r&63)^bit(KeeLoq_NLF,g5(x,1,9,20,26,31)))<<31);
+	}
+	return x;
+}
+
+uint32_t	KeeLoq_Decrypt (const uint32_t data, const uint64_t key)
+{
+	uint32_t	x = data, r;
+
+	for (r = 0; r < 528; r++)
+	{
+		x = (x<<1)^bit(x,31)^bit(x,15)^(uint32_t)bit(key,(15-r)&63)^bit(KeeLoq_NLF,g5(x,0,8,19,25,30));
+	}
+	return x;
+}
+
+
+uint32_t	KeeLoq_Partial_Decrypt(const uint32_t data, const uint64_t key, const uint32_t stop)
+{
+	uint32_t	x = data, r;
+
+	for (r = 0; r < stop; r++)
+	{
+		x = (x<<1)^bit(x,31)^bit(x,15)^(uint32_t)bit(key,(15-r)&63)^bit(KeeLoq_NLF,g5(x,0,8,19,25,30));
+	}
+	return x;
+}
+
+uint32_t popcount_2(uint32_t x)
+{
+    uint32_t m1 = 0x55555555;
+    uint32_t m2 = 0x33333333;
+    uint32_t m4 = 0x0f0f0f0f;
+    x -= (x >> 1) & m1;
+    x = (x & m2) + ((x >> 2) & m2);
+    x = (x + (x >> 4)) & m4;
+    x += x >>  8;
+    return (x + (x >> 16)) & 0x3f;
+}
+
+uint32_t	KeeLoq_Partial_Decrypt_Power (const uint32_t data, const uint64_t key, const uint32_t stop)
+{
+	uint32_t	x = data, r, power, y1,y0;
+    
+	for (r = 0; r < stop; r++)
+	{
+	  if (r == stop - 1)  { y1 = x ; }
+      x = (x<<1)^bit(x,31)^bit(x,15)^(uint32_t)bit(key,(15-r)&63)^bit(KeeLoq_NLF,g5(x,0,8,19,25,30));
+      if (r == stop - 1)  { y0 = x ; }
+	}
+    power = popcount_2(y1^y0) ; 
+	return power;
+}
+
